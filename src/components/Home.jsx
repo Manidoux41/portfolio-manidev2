1f604d7b475fed0a1ac298fe30ff7396b426bd90
Manidoux41 <lherissondu41@gmail.com>
2023-03-13 19:14:16 +0100

    correction home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className="flex h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">Bonjour, je suis Développeur Web</h2>
           <p className="text-gray-500 py-4  max-w-md">
            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus inventore consequuntur neque? Esse aut et perferendis veniam totam laborum alias, itaque reprehenderit ad expedita placeat exercitationem doloribus accusamus unde fugiat!.
           </p>
