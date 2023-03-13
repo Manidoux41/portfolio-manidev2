ca4bccf184b9a56586ede17d817bfcdd2ae0ba4a
Manidoux41 <lherissondu41@gmail.com>
2023-03-13 15:11:45 +0100

    add link to demo and code

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,26 +11,38 @@ const Portfolio = () => {
     {
       id: 1,
       src: booki,
+      demo: 'manidoux41.github.io/booki_v5/',
+      code: 'https://github.com/Manidoux41/booki_v5.git', 
     },
     {
       id: 2,
       src: ohmyfood,
+      demo: 'https://manidoux41.github.io/ohMyfood_v4/',
+      code: 'https://github.com/Manidoux41/ohMyfood_v4.git', 
     },
     {
       id: 3,
       src: lapanthere,
+      demo: 'https://la-panthere-v3.vercel.app',
+      code: 'https://github.com/Manidoux41/la_panthere_v3.git', 
     },
     {
       id: 4,
       src: kanap,
+      demo: 'https://front-p5.vercel.app',
+      code: 'https://github.com/Manidoux41/back-p5.git', 
     },
     {
       id: 5,
       src: pikante,
+      demo: 'https://front-pikante.vercel.app',
+      code: 'https://github.com/Manidoux41/Pikante_v4', 
     },
     {
       id: 6,
       src: kasa,
+      demo: 'https://kasa-v2.vercel.app',
+      code: 'https://github.com/Manidoux41/kasa_v3.git', 
     },
   ];
 
@@ -45,7 +57,7 @@ const Portfolio = () => {
           <p className=" py-6">Check out some of my work right here</p>
         </div>
         <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-        {portfolios.map(({ id, src }) => (
+        {portfolios.map(({ id, src,demo,code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
               <img
                 src={src}
@@ -53,12 +65,12 @@ const Portfolio = () => {
                 className="object-cover rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a href={demo} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a href={code} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
-                </button>
+                </a>
               </div>
             </div>
         ))}
